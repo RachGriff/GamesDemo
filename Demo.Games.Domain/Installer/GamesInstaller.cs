84bc2a041ec8e27030fee252d66348250cd3f48e
carl.griffiths <carl.griffiths@bybox.com>
2019-02-26 16:22:54 +0000

    amend mlab uri

--- Demo.Games.Domain/Installer/GamesInstaller.cs
+++ Demo.Games.Domain/Installer/GamesInstaller.cs
@@ -14,7 +14,7 @@ namespace Demo.Games.Domain.Installer
     {
         public static void AddGames(this IServiceCollection services)
         {
-            var connectionString = "mongodb://rlgriffiths:gamesdemo123@ds123513.mlab.com:23513/gamesdemo";
+            var connectionString = "mongodb://rlgriffiths:gamesdemo123@ds033734.mlab.com:33734/gamesdemo";
             var databaseName = "Games";
             var client=new MongoClient(connectionString);
             var database = client.GetDatabase(databaseName);
