97eb19cce81a0835958c703f5a3d622c4c325672
Rachel <rlgriffiths@hotmail.co.uk>
2019-03-15 17:40:54 +0000

    change to process environment variable rather than machine

--- Demo.Games.Domain/Adapters/Config.cs
+++ Demo.Games.Domain/Adapters/Config.cs
@@ -6,7 +6,7 @@ namespace Demo.Games.Domain.Adapters
 {
     public static class Config
     {
-        public static string MongoDbConnectionString = System.Environment.GetEnvironmentVariable("GAMES_CONNECTIONSTRING",EnvironmentVariableTarget.Machine);
+        public static string MongoDbConnectionString = System.Environment.GetEnvironmentVariable("GAMES_CONNECTIONSTRING",EnvironmentVariableTarget.Process);
 
         
     }
